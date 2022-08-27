384a5c31ca11b845e50674ad50c4f8247b2fd427
Red <me@red.to>
2022-08-27 16:37:52 +0100

    Initial Commit

---
+++ HTTP/HTTP.h
@@ -0,0 +1,18 @@
+//
+//  HTTP.h
+//  HTTP
+//
+//  Created by Red Davis on 27/08/2022.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for HTTP.
+FOUNDATION_EXPORT double HTTPVersionNumber;
+
+//! Project version string for HTTP.
+FOUNDATION_EXPORT const unsigned char HTTPVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <HTTP/PublicHeader.h>
+
+
